f25bdcc4937fb4ab2f57dababdcdfaa875d7d63f
Tj Holowaychuk <tj@vision-media.ca>
2010-12-14 12:40:53 -0800

    Passing options

--- examples/basic.js
+++ examples/basic.js
@@ -6,6 +6,6 @@
 var css = require('../')
   , str = require('fs').readFileSync(__dirname + '/basic.css', 'utf8');
 
-css.render(str, { filename: 'basic.css' }, function(err, css){
+css.render(str, { filename: 'basic.css', compress: true }, function(err, css){
   console.log(css);
 });
