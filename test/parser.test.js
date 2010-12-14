12a46d7def6399416fdefb609de4c31761e62617
Tj Holowaychuk <tj@vision-media.ca>
2010-12-13 20:45:36 -0800

    Initializing Lexer selector context to true

--- test/parser.test.js
+++ test/parser.test.js
@@ -11,7 +11,7 @@ module.exports = {
   'test variable': function(){
     var parser = new Parser('@foo')
       , ast = parser.parse();
-    
+
     ast.should.be.an.instanceof(nodes.Block);
     ast.nodes[0].should.be.an.instanceof(nodes.Variable);
     ast.nodes[0].should.have.property('name', 'foo');
@@ -21,10 +21,23 @@ module.exports = {
   'test variable assignment': function(){
     var parser = new Parser('@light-color: #fff')
       , ast = parser.parse();
-    
+
     ast.should.be.an.instanceof(nodes.Block);
     ast.nodes[0].should.be.an.instanceof(nodes.Variable);
     ast.nodes[0].should.have.property('name', 'light-color');
     ast.nodes[0].val.should.be.an.instanceof(nodes.Color);
+  },
+  
+  'test selector': function(){
+    var parser = new Parser('body a\n  color #fff')
+      , ast = parser.parse();
+
+    ast.should.be.an.instanceof(nodes.Block);
+    var selector = ast.nodes[0];
+    selector.should.be.an.instanceof(nodes.Selector);
+    selector.val.should.equal('body a');
+    var block = selector.block.nodes;
+    block[0].should.be.an.instanceof(nodes.Property);
+    block[0].should.have.property('name', 'color');
   }
 };
