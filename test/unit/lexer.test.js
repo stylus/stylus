87c1a1d1053af76f779916e96e94486b31cf5a98
Tj Holowaychuk <tj@vision-media.ca>
2010-12-24 09:53:07 -0800

    Added failing url() test

--- test/unit/lexer.test.js
+++ test/unit/lexer.test.js
@@ -287,5 +287,12 @@ module.exports = {
     lex.peek.type.should.equal('string');
     lex.next.val.val.should.equal('/path/to some image.png');
     lex.next.type.should.equal(')');
+    
+    var lex = new Lexer('url("/my.png")');
+    lex.next.val.name.should.equal('url');
+    lex.next.type.should.equal('(');
+    lex.peek.type.should.equal('string');
+    lex.next.val.val.should.equal('/my.png');
+    lex.next.type.should.equal(')');
   }
 };
