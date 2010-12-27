ec292d6813b896062210dad18ff58a06690b4901
Tj Holowaychuk <tj@vision-media.ca>
2010-12-27 13:02:31 -0800

    Expose nodes

--- examples/images.js
+++ examples/images.js
@@ -4,10 +4,14 @@
  */
 
 var css = require('../')
+  , nodes = css.nodes
   , str = require('fs').readFileSync(__dirname + '/images.css', 'utf8');
 
 css(str)
   .set('filename', 'images.css')
+  // .define('url', function(url){
+  //   return new nodes.String
+  // })
   .render(function(err, css){
     if (err) throw err;
     console.log(css);
