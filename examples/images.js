04e2a62d7d5a3c1552d3e77e6dc5269cd00ac3c6
Tj Holowaychuk <tj@vision-media.ca>
2010-12-27 14:00:25 -0800

    Added url() image inlineing

--- examples/images.js
+++ examples/images.js
@@ -7,21 +7,9 @@ var css = require('../')
   , nodes = css.nodes
   , str = require('fs').readFileSync(__dirname + '/images.css', 'utf8');
 
-function url() {
-  var n = 0
-    , pending = 0
-    , urls = {};
-  return function(url){
-    ++pending;
-    var tok = '__images[' + ++n + ']__';
-    urls[url] = { url: url, tok: tok };
-    return new nodes.Ident(tok);
-  }
-}
-
 css(str)
   .set('filename', 'images.css')
-  .define('url', url())
+  .define('url', css.url({ paths: [__dirname] }))
   .render(function(err, css){
     if (err) throw err;
     console.log(css);
