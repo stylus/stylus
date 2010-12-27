3faa8e24dd79d2c7cdc41ef5758822e2dae2f5ed
Tj Holowaychuk <tj@vision-media.ca>
2010-12-27 12:46:51 -0800

    Added lib/utils.js

--- examples/images.js
+++ examples/images.js
@@ -6,7 +6,9 @@
 var css = require('../')
   , str = require('fs').readFileSync(__dirname + '/images.css', 'utf8');
 
-css.render(str, { filename: 'images.css' }, function(err, css){
-  if (err) throw err;
-  console.log(css);
-});
+css(str)
+  .set('filename', 'images.css')
+  .render(function(err, css){
+    if (err) throw err;
+    console.log(css);
+  });
