0883533b512659601ca91e68f7ca63eb6f6cedef
Tj Holowaychuk <tj@vision-media.ca>
2011-01-11 10:13:45 -0800

    todo

--- test/integration/test.js
+++ test/integration/test.js
@@ -15,6 +15,7 @@ var css = require('../../')
  */
 
 function test(test) {
+  // TODO: remove
   if ('functions.defaults' == test || 'scope.complex' == test) return;
   var base = __dirname + '/tests/' + test
     , path =  base + '.in'
