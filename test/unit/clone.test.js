73ae5471c7835f60c4c294b28c54948a5793fb36
Tj Holowaychuk <tj@vision-media.ca>
2011-01-13 12:36:35 -0800

    Added Params#clone() test

--- test/unit/clone.test.js
+++ test/unit/clone.test.js
@@ -7,6 +7,10 @@ var nodes = require('../../lib/nodes')
   , should = require('should');
 
 module.exports = {
+  'test default': function(){
+    nodes.null.clone().should.equal(nodes.null);
+  },
+
   'test Color': function(){
     var node = new nodes.Color(255,50,0,1)
       , clone = node.clone();
@@ -27,5 +31,21 @@ module.exports = {
     clone.s.should.equal(50);
     clone.l.should.equal(12);
     clone.a.should.equal(1);
+  },
+  
+  'test Params': function(){
+    var node = new nodes.Params
+      , a = new nodes.Unit(15)
+      , b = new nodes.Unit(20);
+
+    node.nodes.push(a);
+    node.nodes.push(b);
+
+    var clone = node.clone();
+    clone.should.not.equal(node);
+    clone.nodes[0].should.not.equal(a);
+    clone.nodes[1].should.not.equal(b);
+    clone.nodes[0].val.should.equal(15);
+    clone.nodes[1].val.should.equal(20);
   }
 };
