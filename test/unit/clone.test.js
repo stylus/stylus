fd227083ef1469c7550166c2a94ccf3202c216ae
Tj Holowaychuk <tj@vision-media.ca>
2011-01-13 12:32:28 -0800

    Added HSLA#clone() test

--- test/unit/clone.test.js
+++ test/unit/clone.test.js
@@ -16,5 +16,16 @@ module.exports = {
     clone.g.should.equal(50);
     clone.b.should.equal(0);
     clone.a.should.equal(1);
+  },
+  
+  'test HSLA': function(){
+    var node = new nodes.HSLA(100, 50, 12, 1)
+      , clone = node.clone();
+
+    clone.should.not.equal(node);
+    clone.h.should.equal(100);
+    clone.s.should.equal(50);
+    clone.l.should.equal(12);
+    clone.a.should.equal(1);
   }
 };
