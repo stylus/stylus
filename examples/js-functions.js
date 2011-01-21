15841e1c0688da3cdfe52e085155fb1b798dd97b
Tj Holowaychuk <tj@vision-media.ca>
2011-01-21 01:47:05 -0800

    Added image-size() example

--- examples/js-functions.js
+++ examples/js-functions.js
@@ -5,7 +5,8 @@
 
 var css = require('../')
   , nodes = css.nodes
-  , str = require('fs').readFileSync(__dirname + '/js-functions.styl', 'utf8');
+  , str = require('fs').readFileSync(__dirname + '/js-functions.styl', 'utf8')
+  , fs = require('fs');
 
 function add(a, b) {
   return a.operate('+', b);
@@ -15,10 +16,35 @@ function sub(a, b) {
   return a.operate('-', b);
 }
 
+function imageSize(img) {
+  // assert that the node (img) is a String node, passing
+  // the param name for error reporting
+  css.utils.assertType(img, nodes.String, 'img');
+  var path = img.val
+    , data = fs.readFileSync(__dirname + '/' + path);
+
+  // GIF
+  // of course you would support.. more :)
+  if ('GIF' == data.slice(0, 3).toString()) {
+    var w = data.slice(6, 8)
+      , h = data.slice(8, 10);
+    w = w[1] << 8 | w[0];
+    h = h[1] << 8 | h[0];
+  }
+
+  // Return a tuple (aka expression)
+  var expr = new nodes.Expression;
+  expr.push(new nodes.Unit(w));
+  expr.push(new nodes.Unit(h));
+
+  return expr;
+}
+
 css(str)
   .set('filename', 'js-functions.styl')
   .define('add', add)
   .define('sub', sub)
+  .define('image-size', imageSize)
   .render(function(err, css){
     if (err) throw err;
     console.log(css);
