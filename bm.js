3089365fed926e23b1c17c6e47b9c1f3cfbdf343
Tj Holowaychuk <tj@vision-media.ca>
2011-03-10 11:59:30 -0800

    Added bm.js

---
+++ bm.js
@@ -0,0 +1,22 @@
+
+/**
+ * Module dependencies.
+ */
+
+var stylus = require('./');
+
+var times = 200
+  , n = times
+  , start = new Date;
+
+console.log('compiling %d times', times);
+
+while (n--) {
+  stylus('body\n  color: white;\n  background: url(/images/foo.png);')
+    .render(function(err, css){});
+}
+
+var duration = new Date - start;
+console.log('  duration: %dms', duration);
+console.log('  average: %dms', duration / times);
+console.log('  per second: %d', (times / (duration / 1000)).toFixed(1));
