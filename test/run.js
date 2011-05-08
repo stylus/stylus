d9e9798ec5957454ecaea445a3957678ed9e15d1
Tj Holowaychuk <tj@vision-media.ca>
2011-05-08 10:23:03 -0700

    Fixed 0% in @keyframes from becoming "0" when compressed. Closes #248

--- test/run.js
+++ test/run.js
@@ -45,8 +45,7 @@ function test(test, fn) {
       if (err) throw err;
       fs.readFile(csspath, 'utf8', function(err, expected){
         if (err) throw err;
-        expected += '\n';
-        if (actual == expected) {
+        if (actual.trim() == expected.trim()) {
           fn();
         } else {
           fn(actual, expected);
