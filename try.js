236f90dbd4dcc6a76b7959f51667e680155ecac7
Tj Holowaychuk <tj@vision-media.ca>
2011-09-29 20:01:58 -0700

    setting up code mirror

---
+++ try.js
@@ -0,0 +1,27 @@
+
+$(function(){
+  $('.stylus').each(function(){
+    var css = $(this).next('.css').get(0)
+      , styl = this;
+
+    styl = CodeMirror.fromTextArea(styl, {
+        lineNumbers: true
+      , onKeyEvent: render
+    });
+
+    css = CodeMirror.fromTextArea(css, {
+      lineNumbers: true
+    });
+
+    function render() {
+      var str = styl.getValue();
+      stylus(str)
+        .render(function(err, str){
+          // if (err) throw err;
+          css.setValue(str);
+        });
+    }
+
+    render();
+  });
+});
