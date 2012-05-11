32166b873ff877c75d786979f16e5e1bc9aa7e83
kpdecker <kpdecker@gmail.com>
2012-05-11 01:20:49 -0500

    Break import tests into separate test. DRY up code

--- test/run.js
+++ test/run.js
@@ -16,13 +16,17 @@ var cases = fs.readdirSync('test/cases').filter(function(file){
 
 describe('integration', function(){
   cases.forEach(function(test){
-    var name = test.replace(/[-.]/g, ' ');
-    it(name, function(){
-      var path = 'test/cases/' + test + '.styl';
-      var styl = fs.readFileSync(path, 'utf8').replace(/\r/g, '');
-      var css = fs.readFileSync('test/cases/' + test + '.css', 'utf8').replace(/\r/g, '').trim();
+    var name = test.replace(/[-.]/g, ' ')
+      , path = 'test/cases/' + test + '.styl'
+      , styl
+      , css;
 
-      var style = stylus(styl)
+    function load() {
+      styl = styl || fs.readFileSync(path, 'utf8').replace(/\r/g, '');
+      css = css || fs.readFileSync('test/cases/' + test + '.css', 'utf8').replace(/\r/g, '').trim();
+    }
+    function setup(styl, options) {
+      var style = stylus(styl, options)
         .set('filename', path)
         .include(__dirname + '/images')
         .include(__dirname + '/cases/import.basic')
@@ -31,47 +35,39 @@ describe('integration', function(){
       if (~test.indexOf('compress')) style.set('compress', true);
       if (~test.indexOf('include')) style.set('include css', true);
 
+      return style;
+    }
+    function run(style) {
       style.render(function(err, actual){
         if (err) throw err;
         actual.trim().should.equal(css);
       });
+    }
 
-      if (test === 'introspection') {
-        // By design this does not work for the import case,
-        // ignore the operation.
-        return;
-      }
+    it(name, function(){
+      load();
 
-      // Rerun as an import to ensure the same functionality
+      var style = setup(styl);
+      run(style);
+    });
+
+    if (test === 'introspection') {
+      // By design this does not work for the import case,
+      // ignore the operation.
+      return;
+    }
+
+    // Rerun as an import to ensure the same functionality
+    it(name + ' import', function(){
       var importCache = {};
-      var style = stylus('@import("' + path + '")', {_importCache: importCache})
-        .set('filename', path)
-        .include(__dirname + 'test/cases')
-        .include(__dirname + '/images')
-        .include(__dirname + '/cases/import.basic')
-        .define('url', stylus.url());
+      load();
 
-      if (~test.indexOf('compress')) style.set('compress', true);
-
-      style.render(function(err, actual){
-        if (err) throw err;
-        actual.trim().should.equal(css);
-      });
+      var style = setup('@import("' + path + '")', {_importCache: importCache});
+      run(style);
 
       // And once more for cached imports testing
-      var style = stylus('@import("' + path + '")', {_importCache: importCache})
-        .set('filename', path)
-        .include(__dirname + 'test/cases')
-        .include(__dirname + '/images')
-        .include(__dirname + '/cases/import.basic')
-        .define('url', stylus.url());
-
-      if (~test.indexOf('compress')) style.set('compress', true);
-
-      style.render(function(err, actual){
-        if (err) throw err;
-        actual.trim().should.equal(css);
-      });
+      var style = setup('@import("' + path + '")', {_importCache: importCache});
+      run(style);
     })
   });
 })
